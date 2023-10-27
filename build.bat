a8da269b2200e341af09e32beabb8520162e5082
Jessica Perdomo <j.perdomoalves@gmail.com>
2023-10-27 16:02:47 -0300

    Implemented the diff lines

--- build.bat
+++ build.bat
@@ -1,21 +1,23 @@
 @echo off
 
-REM
+REM Compilar o programa
 g++ -o programa beeAqueleLa.cpp
 
 REM Redireciona a entrada para o programa a partir do arquivo in.txt
 < in.txt programa > out.txt
 
-REM Compara o arquivo de saída com o arquivo de referência
-fc out.txt out_ref.txt > nul
+REM Comparar e armazenar as diferenças em um arquivo diff.txt
+fc /t /l out.txt out_ref.txt > diff.txt
 
-if errorlevel 1 (
-    echo Arquivos sao diferentes.
-) else (
+REM Ver se teve diferenças encontradas e quais são
+if %errorlevel% EQU 0 (
     echo Arquivos sao iguais.
+) else (
+    echo Arquivos sao diferentes. Diferencas encontradas:
+    type diff.txt
 )
 
-REM Limpa os arquivos intermediários
-del programa out.txt
+REM Limpar os arquivos intermediários
+del programa out.txt diff.txt
 
 pause
